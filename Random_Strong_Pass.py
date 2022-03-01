d84d4fd30ce7fcaa6dc81304acf0423c94191f11
Arpan <arpan.pal1966@gmail.com>
2022-03-01 15:47:16 +0530

    Added Comments

--- Random_Strong_Pass.py
+++ Random_Strong_Pass.py
@@ -1,17 +1,19 @@
 import random
 print("Welcome to your Password Generator")
+
+#characters used in making the password
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`! @#$%^&*()_-+={[}]|\:;<,>.?/"
 
 num = input("How many Passwords you want to generate? ")
-num = int(num)
+num = int(num)  #inputing number of passwords to generate
 
 len = input("What length of password would you like to generate? ")
-len = int(len)
+len = int(len)   #inputing length of password to generate
 
 print("\n Here are your unique and strong password Sir -")
 
-for pwd in range(num):
-    passwords = ""
+for pwd in range(num):  
+    passwords = ""    #storing the randomly generated password
     for c in range(len):
         passwords += random.choice(chars)
     print(passwords)
